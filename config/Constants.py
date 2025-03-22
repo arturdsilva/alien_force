2f697f2c2ea8e1ed5cdf397c864c94551eff7562
arturdsilva <artur1512002@gmail.com>
2025-03-22 17:36:39 -0300

    feat: class to represent different colors

--- config/Constants.py
+++ config/Constants.py
@@ -6,5 +6,20 @@ class Constants:
     PLAYER_HEIGHT = 100
     PLAYER_SPEED = 100
 
-    # Colors
+
+class Colors:
     WHITE = (255, 255, 255)
+    BLACK = (0, 0, 0)
+    RED = (255, 0, 0)
+    GREEN = (0, 255, 0)
+    BLUE = (0, 0, 255)
+    YELLOW = (255, 255, 0)
+    CYAN = (0, 255, 255)
+    MAGENTA = (255, 0, 255)
+    ORANGE = (255, 165, 0)
+    PURPLE = (128, 0, 128)
+    GRAY = (128, 128, 128)
+    DARK_GRAY = (64, 64, 64)
+    LIGHT_GRAY = (200, 200, 200)
+    BROWN = (139, 69, 19)
+    PINK = (255, 192, 203)
